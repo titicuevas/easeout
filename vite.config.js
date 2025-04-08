e3b1ce7b95554b5e9ff94271cdd5a141c0434d1d
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2025-04-08 17:42:20 +0200

    Va mejorando el Railways

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,25 @@ export default defineConfig({
             input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        react(),
+        react({
+            // Configuración específica para React
+            jsxRuntime: 'automatic',
+            babel: {
+                plugins: ['@babel/plugin-syntax-jsx'],
+            },
+        }),
     ],
+    server: {
+        https: process.env.APP_ENV === 'production',
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        }
+    },
+    build: {
+        // Usa la URL base según el entorno
+        base: process.env.APP_ENV === 'production' 
+            ? 'https://easeout-production.up.railway.app'
+            : '/',
+    }
 });
